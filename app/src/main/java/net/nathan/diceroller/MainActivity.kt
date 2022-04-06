ece152e41f70342986b821dab7001da1ae6b5d9f
kazaku lopez nathan <19k212@esisalama.org>
2022-04-06 02:54:56 +0200

    ending of this part of codelab

--- app/src/main/java/net/nathan/diceroller/MainActivity.kt
+++ app/src/main/java/net/nathan/diceroller/MainActivity.kt
@@ -2,10 +2,33 @@ package net.nathan.diceroller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+
+
+        rollButton.setOnClickListener{
+            //Toast.makeText(this, "roll : ${r}", Toast.LENGTH_SHORT).show()
+            roll()
+        }
+    }
+    private fun roll(){
+        val side = 6
+        val diceText: TextView = findViewById(R.id.mytext)
+        val dice = Dice(side)
+
+        diceText.text = dice.roll().toString()
+    }
+}
+
+class Dice(private val side: Int){
+    fun roll() : Int{
+        return (1..side).random()
     }
 }
