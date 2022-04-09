9db5e75b7a2449457747acabbe0c704a6a448837
kazaku lopez nathan <19k212@esisalama.org>
2022-04-10 01:58:55 +0200

    ending of this part of codelab

--- app/src/test/java/net/nathan/diceroller/ExampleUnitTest.kt
+++ app/src/test/java/net/nathan/diceroller/ExampleUnitTest.kt
@@ -1,17 +1,14 @@
 package net.nathan.diceroller
 
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
-import org.junit.Assert.*
-
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-class ExampleUnitTest {
+class DiceRollTest{
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun generates_number(){
+        val dice = Dice(6)
+        val rollResult = dice.roll()
+        assertTrue("the value of rollResult must is between 1 and 6", rollResult in 1..6)
     }
+
 }
